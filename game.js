9481734e21c1d6c9b901e470de71e376cf915f5c
Cole Berman <11coleandrew4@gmail.com>
2022-10-11 16:01:41 -0500

    Snake Game is fully functional!

--- game.js
+++ game.js
@@ -2,18 +2,28 @@ import {
   SNAKE_SPEED,
   update as updateSnake,
   draw as drawSnake,
+  getSnakeHead,
+  snakeIntersection,
 } from './snake.js';
+
+import { outsideGrid } from './grid.js';
 
 import { update as updateFood, draw as drawFood } from './food.js';
 
 let lastRenderTime = 0;
+let gameOver = false;
 const gameBoard = document.getElementById('game-board');
 function main(currentTime) {
+  if (gameOver) {
+    if (confirm('You lost. Press OK to restart.')) {
+      window.location = '/';
+    }
+    return;
+  }
   window.requestAnimationFrame(main);
   const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
   if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
 
-  console.log('render');
   lastRenderTime = currentTime;
   update();
   draw();
@@ -24,10 +34,15 @@ window.requestAnimationFrame(main);
 function update() {
   updateSnake();
   updateFood();
+  checkDeath();
 }
 
 function draw() {
   gameBoard.innerHTML = '';
   drawSnake(gameBoard);
   drawFood(gameBoard);
+}
+
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
 }
